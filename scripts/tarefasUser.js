fe6e5d3dfda238759281007ec9a37735d1e9074a
nomeUsuario <joeljerrys95@gmail.com>
2022-04-12 16:45:05 -0300

    Inclusão do botão editar, e função de conclusao de tarefas pendente

--- scripts/tarefasUser.js
+++ scripts/tarefasUser.js
@@ -3,12 +3,16 @@ let closeAppRef = document.querySelector('#closeApp')
 let newTaskRef = document.querySelector('#newTask')
 let skeletonRef = document.querySelector('#skeleton')
 let ulTarefasRef = document.querySelector('.tarefas-pendentes')
+let ulTarefasTerminadasRef = document.querySelector('.tarefas-terminadas')
 let novaTarefaRef = document.querySelector('#novaTarea')
 let requestHeaders = {
     "Content-Type": "application/json",
     Authorization: localStorage.getItem("token")
 }
 let taskList = []
+let tasksCompletedList = []
+console.log(taskList)
+console.log(tasksCompletedList)
 
 //faz o logout da pagina de tarefas
 function logOutUser() {
@@ -18,7 +22,7 @@ function logOutUser() {
 
 function taskRender() {
 
-    for (const task of taskList.reverse()) {
+    for (const task of taskList) {
         const dataCreat = new Date(task.createdAt)
         const dataFormatada = dataCreat.toLocaleDateString(
             'pt-br',
@@ -31,13 +35,34 @@ function taskRender() {
         )
             ulTarefasRef.innerHTML += `
             <li class="tarefa">
-            <div class="not-done"></div>
+            <div class="not-done" onclick="taskCompleted(${task.id})"></div>
             <div class="descricao">
               <p class="nome">${task.description}</p>
-              <p class="timestamp">${dataFormatada}</p>
+              <p class="timestamp">Criada em: ${dataFormatada}</p>
+              <p id="closeApp" onclick="EditTarefa(${task.id})">Editar</p>
               <p id="closeApp" onclick="excluir(${task.id})">Excluir</p>
             </div>
           </li>`    
+    }
+    for (const task of tasksCompletedList) {
+        const dataCreat = new Date(task.createdAt)
+        const dataFormatada = dataCreat.toLocaleDateString(
+            'pt-br',
+            {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric'
+                
+            }
+        )
+            ulTarefasTerminadasRef.innerHTML += `
+          <li class="tarefa">
+            <div class="not-done" onclick="taskCompletedReturn(${task.id})"></div>
+            <div class="descricao">
+              <p class="nome">${task.description}</p>
+              <p class="timestamp">Criada em: ${dataFormatada}</p>
+            </div>
+          </li>`        
     }
     
 }
@@ -62,6 +87,80 @@ function creatTask() {
         response.json()
         
     });
+   
+}
+function taskCompletedReturn(id) {
+    
+    let taskCompletedReturn = {
+        
+        completed: false
+    }
+    
+    let requestConfigurationPut = {
+        method: "PUT",
+        body: JSON.stringify(taskCompletedReturn),
+        headers: requestHeaders
+    }
+
+    fetch(
+        `https://ctd-todo-api.herokuapp.com/v1/tasks/${id}`,
+        requestConfigurationPut
+    ).then(response => {
+        response.json()
+        
+    });
+    location.reload()
+    alert('Tarefa de volta para pendentes')
+   
+}
+function taskCompleted(id) {
+    
+    let taskCompleted = {
+        
+        completed: true
+    }
+    
+    let requestConfigurationPut = {
+        method: "PUT",
+        body: JSON.stringify(taskCompleted),
+        headers: requestHeaders
+    }
+
+    fetch(
+        `https://ctd-todo-api.herokuapp.com/v1/tasks/${id}`,
+        requestConfigurationPut
+    ).then(response => {
+        response.json()
+        
+    });
+    location.reload()
+    alert('Parabéns por concluir a tarefa!')
+   
+}
+
+
+function EditTarefa(id) {
+
+    let EditTask = {
+        description: novaTarefaRef.value,
+        completed: false
+    }
+    
+    let requestConfigurationPut = {
+        method: "PUT",
+        body: JSON.stringify(EditTask),
+        headers: requestHeaders
+    }
+
+    fetch(
+        `https://ctd-todo-api.herokuapp.com/v1/tasks/${id}`,
+        requestConfigurationPut
+    ).then(response => {
+        response.json()
+        
+    });
+    location.reload()
+    alert('Tarefa Editada Com sucesso!')
    
 }
 
@@ -109,12 +208,13 @@ function atualizar(){
             //console.log(data)
     
             for (const task of data) {
-    
-                taskList.push(task)
+                
+                if(task.completed){
+                    tasksCompletedList.push(task)
+                }else taskList.push(task)
     
             }
-            console.log(taskList)
-    
+                
             skeletonRef.style.display = "none";
             taskRender()
             
